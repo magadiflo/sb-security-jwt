1ccaf41000a22e09dcdbe3e487868229272feb4f
Martín <magadiflo@gmail.com>
2022-09-05 12:30:37 -0500

    Inicio del proyecto - Spring Boot Security con JWT

---
+++ README.md
@@ -0,0 +1,3 @@
+# [Spring Boot Token based Authentication with Spring Security & JWT](https://www.bezkoder.com/spring-boot-jwt-authentication/)
+Tomado de la página web **BezKoder**
+
---
+++ src/test/java/com/magadiflo/SbSecurityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magadiflo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbSecurityJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
